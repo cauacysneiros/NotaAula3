197b2f2714e71d9438cc870018874b7deecb1f48
cauacysneiros <164400378+cauacysneiros@users.noreply.github.com>
2024-09-30 16:08:10 -0300

    Add files via upload

---
+++ NotaAula3/Questao1/Gerente.java
@@ -0,0 +1,21 @@
+package NotaAula3.Questao1;
+
+public class Gerente extends Funcionario{
+
+    private String projeto;
+
+    public String getProjeto(){
+        return projeto;
+    }
+
+    public void setProjeto(String projeto){
+        this.projeto = projeto;
+    }
+
+    public void informarProjeto(){
+        System.out.println("Informe o projeto: ");
+        setProjeto(scanner2.nextLine());
+
+    }
+
+}
