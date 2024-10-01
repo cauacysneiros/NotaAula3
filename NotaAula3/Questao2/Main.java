8aceac1e107f1a6c85d8bc1abd31b9d17dcec0c1
cauacysneiros <164400378+cauacysneiros@users.noreply.github.com>
2024-10-01 18:40:55 -0300

    Main.java

--- NotaAula3/Questao2/Main.java
+++ NotaAula3/Questao2/Main.java
@@ -6,14 +6,17 @@ public class Main {
         Cachorro c = new Cachorro();
         Gato g = new Gato("Inácio", "Siâmes", "caminha", "mia");
 
-        Scanner scanner = new Scanner(System.in);
-        Scanner scanner2 = new Scanner(System.in);
-
+        Scanner sc = new Scanner(System.in);
+        
         System.out.println("Informações do Cachorro:");
-        System.out.println("Nome: " + c.setNome());
-        System.out.println("Raça: " + c.setRaca());
-        System.out.println("Locomoção: " + c.setLocomocao());
-        System.out.println("Late: " + c.setLate());
+        System.out.println("Nome: ");
+        c.setNome(sc.nextLine());
+        System.out.println("Raça: ");
+        c.setRaca(sc.nextLine());
+        System.out.println("Locomoção: ") ;
+        c.setLocomocao(sc.nextLine());
+        System.out.println("Late: ");
+        c.setLate(sc.nextLine());
 
         System.out.println("Informações da Gato:");
         System.out.println("Nome: " + g.getNome());
